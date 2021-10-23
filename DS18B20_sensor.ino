eeb2d2cbdcf96c0677a53c51c762a21971199712
LeonardoRedaelli1999 <92178641+LeonardoRedaelli1999@users.noreply.github.com>
2021-10-23 23:53:17 +0200

    Update DS18B20_sensor.ino

--- DS18B20_sensor.ino
+++ DS18B20_sensor.ino
@@ -8,8 +8,9 @@
 #define DHT_SENSOR_TYPE DHT_TYPE_11
 // Data wire bus for DS18B20 in digital pin 2
 #define ONE_WIRE_BUS 2
-// Humidity sensor pin 3
-#define DHT_SENSOR_PIN 3
+// Humidity sensor DHT INSIDE (pin 3) and OUTSIDE (pin 4) the beehive
+#define DHT_SENSOR_IN_PIN 3
+#define DHT_SENSOR_OUT_PIN 4
 
 // Setup a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);  
@@ -17,19 +18,23 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 
-//number of temperature/humidity sensors PREDEFINED
+// Number of temperature/humidity sensors PREDEFINED
 const int numDS18B20 = 5;
 const int numDHT = 2;
 float tempC[numDS18B20];
 float temperature[numDHT];
 float humidity[numDHT];
 
-//need to implement the sensors as an array
-DHT_nonblocking dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+// Initialize the DHT sensors
+DHT_nonblocking dht_sensor_in(DHT_SENSOR_IN_PIN, DHT_SENSOR_TYPE);
+DHT_nonblocking dht_sensor_out(DHT_SENSOR_OUT_PIN, DHT_SENSOR_TYPE);
+
 
 void setup(void)
 {
-  sensors.begin();  // Start up the library for DS18B20
+  // Start up the library for DS18B20
+  sensors.begin();
+  
   Serial.begin(9600);
   
 }
@@ -39,25 +44,39 @@ void loop(void)
   // Send command to all DS18B20 for temperature conversion
   sensors.requestTemperatures();
   
-  // Display temperature from each sensor
+  // Get temperature from each sensor and display it in order to test/debug
   for (int i = 0;  i < numDS18B20;  i++)
   {
-    Serial.print("Sensor ");
-    Serial.print(i+1);
-    Serial.print(" : ");
+    //Serial.print("Sensor ");
+    //Serial.print(i+1);
+    //Serial.print(" : ");
     tempC[i] = sensors.getTempCByIndex(i);
-    Serial.print(tempC[i]);
-    Serial.print("C");
+    //Serial.print(tempC[i]);
+    //Serial.print("C");
   }
   
   Serial.println("");
   
-  if(dht_sensor.measure(&temperature, &humidity)){
+  // Returns true if a measurement is available
+  if(dht_sensor_in.measure(&temperature, &humidity)){
     Serial.print( "T = " );
     Serial.print( temperature, 1 );
     Serial.print( " deg. C, H = " );
     Serial.print( humidity, 1 );
     Serial.println( "%" );
+  }
+  //else{
+    //TRY FOR SOME TIME AND IF IT DOESN'T RESPOND THAN ALWAYS RETURN AN ERROR VALUE AND CALL IT NO MORE
+  //}
+  
+  //NOT SURE WHETHER IT'S JUST A +1 TO GIVE HIM THE POINTER TO THE NEXT CELL OF THE ARRAY
+  if(dht_sensor_out.measure(&temperature+1, &humidity+1)){
+    Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+  }
     
   delay(1000);
 }
