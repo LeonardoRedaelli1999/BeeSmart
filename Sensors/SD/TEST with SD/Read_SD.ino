c5f8891b91dc6d227c593c41b76d017252ab24be
GianGeneraldo <92991836+GianGeneraldo@users.noreply.github.com>
2021-10-29 16:58:36 +0200

    Create Read_SD.ino

---
+++ Sensors/SD/TEST with SD/Read_SD.ino
@@ -0,0 +1,38 @@
+#include <SPI.h>
+//This code is the same as the one in 'Generico code for SD', we copied the code here just to make it easy for the reader to understand
+
+#include <SD.h>
+File myFile;
+void setup() {
+// Open serial communications and wait for port to open:
+Serial.begin(9600);
+  
+  // Serial = TRUE if connection is established
+while (!Serial) {
+; // wait for serial port to connect. Needed for native USB port only
+}
+// Serial.print("Initializing SD card...");
+  // Initializing the SD
+if (!SD.begin(10)) {
+Serial.println("initialization failed!");
+while (1);
+}
+Serial.println("initialization done.");
+// open the file for reading:
+myFile = SD.open("test.txt");
+if (myFile) {
+Serial.println("test.txt:");
+// read from the file until there's nothing else in it:
+while (myFile.available()) {
+Serial.write(myFile.read());
+}
+// close the file:
+myFile.close();
+} else {
+// if the file didn't open, print an error:
+Serial.println("error opening test.txt");
+}
+}
+void loop() {
+// nothing happens after setup
+}
