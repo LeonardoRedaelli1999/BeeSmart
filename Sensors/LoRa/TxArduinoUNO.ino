fc257ab68eba4399347b22960a7c2ff1b8f13e8d
LeonardoRedaelli1999 <92178641+LeonardoRedaelli1999@users.noreply.github.com>
2021-11-15 19:45:01 +0100

    Update TxArduinoUNO.ino

--- Sensors/LoRa/TxArduinoUNO.ino
+++ Sensors/LoRa/TxArduinoUNO.ino
@@ -14,15 +14,16 @@
 // Sensor type for the humidity sensor
 #define DHTTYPE DHT22   // DHT 22
 // Humidity sensor DHT INSIDE pin 3 the beehive
-#define DHT_IN_PIN 3
+#define DHT_PIN 3
 // Data wire bus for DS18B20 in digital pin 4
 #define ONE_WIRE_BUS 4 // BRUTTO TESTARE
-#define PIR_PIN 5
+#define PIR_PIN1 5
+#define PIR_PIN2 6
 #define LEDpin 13
 //LoRa Ra-02 pin connections: GND->GND, 3.3V->3.3V, RST->D9, DIO0->D2, NSS->D10, MOSI->D11, MISO->D12, SCK->D13
 
 // Setup a oneWire instance to communicate with any OneWire device
-OneWire oneWire(ONE_WIRE_BUS); //
+OneWire oneWire(ONE_WIRE_BUS);
 // Pass oneWire reference to DallasTemperature library for DS18B20
 DallasTemperature sensors(&oneWire);
 
@@ -30,10 +31,9 @@ DallasTemperature sensors(&oneWire);
 // Number of TEMPERATURE sensors PREDEFINED
 const int numDS18B20 = 5;
 float MaxCount=1200;
-int PIRread = 0;
-
-// BRUTTO float numbers require 2byte, we could save the measurements instantly as integer types (int=measurement*100)
-int tempC[numDS18B20]; //12 significant bits
+int PIRread1 = 0;
+int PIRread2 = 0;
+int tempC[numDS18B20];
 
 // BRUTTO Addresses of DS18B20 sensors retrived with the Sensor_Identifier_DS18B20.ino program. This procedure can be AUTOMATIZED for a large scale production 
 DeviceAddress address_T1 = {0x28, 0x96, 0x25, 0x46, 0x5F, 0x20, 0x01, 0x65}; // T1=CLUSTER of bees
@@ -42,26 +42,21 @@ DeviceAddress address_T3 = {0x28, 0xE2, 0x8A, 0x7A, 0x62, 0x20, 0x01, 0xE9}; // 
 DeviceAddress address_T4 = {0x28, 0x65, 0x7B, 0x5C, 0x5F, 0x20, 0x01, 0x5A}; // T4=CLUSTER of bees (safety)
 DeviceAddress address_T5 = {0x28, 0x49, 0x7C, 0x51, 0x5F, 0x20, 0x01, 0x5D}; // T5=OUTSIDE
 
-
-// Number of HUMIDITY sensors PREDEFINED
-//const int numDHT = 1;
-
 //float temperature[numDHT]; // Temperature measured from the DHT, not needed since we used sensors T1 T2 T3 T4 T5
-//float humidity[numDHT];
+// BRUTTO we need to remove 1 DS18B20 sensor and measure temperature from DHT22
 int humidity;
 
 // Initialize the DHT sensors INSIDE and OUTSIDE the beehive
-DHT_Unified dht_in(DHT_IN_PIN, DHTTYPE);
-//DHT_Unified dht_out(DHT_OUT_PIN, DHTTYPE);
+DHT_Unified dht(DHT_PIN, DHTTYPE);
 
 
 void setup(void)
 {
   // Start up the library for DS18B20 and for DHT
   sensors.begin();
-  dht_in.begin();
-  //dht_out.begin();
-  pinMode(PIR_PIN, INPUT);
+  dht.begin();
+  pinMode(PIR_PIN1, INPUT);
+  pinMode(PIR_PIN2, INPUT);
   Serial.begin(9600);
   
   // Check visually for the power light (spia di accensione)
@@ -84,8 +79,10 @@ void loop(void)
   
   // PIR sensor "number" of detections=detect and "number" of nothing=tool used to have the same time (a discapito della battery)
   int count=0;
-  int detect=0;
   int nothing=0;
+  int detect1=0;
+  int detect2=0;
+  
   
   // Send command to all DS18B20 for temperature conversion
   sensors.requestTemperatures();
@@ -171,19 +168,30 @@ void loop(void)
   // In order for this to work we had to estimate the time on which the PIR was IN ALLARM (~we set it to 1.6s manually) and the time on which the alarm was BLOCKED (~3s from datasheet)
   // We used the program PIR_alarm_stop_time.ino and estimated the time through a mean of time shown at the Serial Monitor
   while (count<MaxCount){
-    PIRread = digitalRead(PIR_PIN);  // read PIR value
-    if (PIRread == HIGH) { 
+    PIRread1 = digitalRead(PIR_PIN1);  // read PIR1 value
+    PIRread2 = digitalRead(PIR_PIN2);  // read PIR2 value
+    if (PIRread1 == HIGH && PIRread2 == HIGH) { 
         delay(100);
-        detect=detect+1;
-      }
-      else{
+        detect1=detect1+1;
+        detect2=detect2+1;
+      } else if (PIRread1 == HIGH && PIRread2 == LOW) {
         delay(100);
+        detect1=detect1+1;
+        nothing=nothing+1;
+      } else if (PIRread1 == LOW && PIRread2 == HIGH) {
+        delay(100);
+        detect2=detect2+1;
+        nothing=nothing+1;
+      } else if (PIRread1 == LOW && PIRread2 == LOW) {
+        delay(100);
+        nothing=nothing+1;
         nothing=nothing+1;
       }
     count=count+1;
   }
 
-    Serial.println(detect/MaxCount);
+    Serial.println(detect1/MaxCount);
+    Serial.println(detect2/MaxCount);
 
   
   // send packets of data, 1 packet each measure
