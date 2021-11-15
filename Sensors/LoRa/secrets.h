1896f169671a5eff7bffee3e8f24313231392f5f
LeonardoRedaelli1999 <92178641+LeonardoRedaelli1999@users.noreply.github.com>
2021-11-15 20:24:49 +0100

    Add files via upload

---
+++ Sensors/LoRa/secrets.h
@@ -0,0 +1,19 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID1 000000			// replace 0000000 with your channel number
+#define SECRET_CH_ID2 000000
+#define SECRET_CH_ID3 000000
+#define SECRET_CH_ID4 000000
+#define SECRET_CH_ID5 000000
+#define SECRET_CH_ID6 000000
+#define SECRET_CH_ID7 000000
+#define SECRET_CH_ID8 000000
+#define SECRET_CH_ID9 000000
+#define SECRET_CH_ID10 000000
+// .........
+
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
