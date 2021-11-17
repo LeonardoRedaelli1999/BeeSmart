7756abede7c986022590dbd99a0cce9f00a2603e
LeonardoRedaelli1999 <92178641+LeonardoRedaelli1999@users.noreply.github.com>
2021-11-17 18:36:08 +0100

    Add files via upload

--- Sensors/SD/TEST with SD/TestSD_DS18B20_DHT_PIR.ino
+++ Sensors/SD/TEST with SD/TestSD_DS18B20_DHT_PIR.ino
@@ -15,13 +15,14 @@ File myFile;
 // Initialize the commands for SLEEP to save energy (from course slides: Enerlib.h)
 
 // Sensor type for the humidity sensor
-#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22
 // Data wire bus for DS18B20 in digital pin 2
 #define ONE_WIRE_BUS 2
 // Humidity sensor DHT INSIDE (pin 3) and OUTSIDE (pin 4) the beehive
 #define DHT_IN_PIN 3
 //#define DHT_OUT_PIN 4
-#define PIR_PIN 5
+#define PIR_PIN1 5 // Fiocco NERO, tempo di attivazione ~1.6s e bloccato ~3s
+#define PIR_PIN2 6 // Fiocco BIANCO, tempo di attivazione ~1.2s e bloccato ~4.1s
 #define LEDpin 13
 
 // Setup a oneWire instance to communicate with any OneWire device
@@ -32,8 +33,9 @@ DallasTemperature sensors(&oneWire);
 
 // Number of TEMPERATURE sensors PREDEFINED
 const int numDS18B20 = 5;
-float MaxCount=10000000;
-int PIRread = 0;
+float MaxCount=1200;
+int PIRread1 = 0;
+int PIRread2 = 0;
 
 // BRUTTO float numbers require 2byte, we could save the measurements instantly as integer types (int=measurement*100)
 float tempC[numDS18B20]; //12 significant bits
@@ -44,10 +46,6 @@ DeviceAddress address_T2 = {0x28, 0x3E, 0xA8, 0x93, 0x63, 0x20, 0x01, 0x5A}; // 
 DeviceAddress address_T3 = {0x28, 0xE2, 0x8A, 0x7A, 0x62, 0x20, 0x01, 0xE9}; // T3=RIGHT or OVEST STOCKS
 DeviceAddress address_T4 = {0x28, 0x65, 0x7B, 0x5C, 0x5F, 0x20, 0x01, 0x5A}; // T4=CLUSTER of bees (safety)
 DeviceAddress address_T5 = {0x28, 0x49, 0x7C, 0x51, 0x5F, 0x20, 0x01, 0x5D}; // T5=OUTSIDE
-
-
-// Number of HUMIDITY sensors PREDEFINED
-//const int numDHT = 2;
 
 //float temperature[numDHT]; // Temperature measured from the DHT, not needed since we used sensors T1 T2 T3 T4 T5
 //float humidity[numDHT];
@@ -64,18 +62,19 @@ void setup(void)
   sensors.begin();
   dht_in.begin();
   //dht_out.begin();
-  pinMode(PIR_PIN, INPUT);
+  pinMode(PIR_PIN1, INPUT);
+  pinMode(PIR_PIN2, INPUT);
   Serial.begin(9600);
   
   // Check visually for the power light (spia di accensione)
-  pinMode(LEDpin, OUTPUT);
-  digitalWrite(LEDpin, HIGH);
+  //pinMode(LEDpin, OUTPUT);
+  //digitalWrite(LEDpin, HIGH);
   delay(3000);
   if (!SD.begin(10)) {
     // If SD initialization didn't go as planned the light stay on
     while (1);
   }
-  digitalWrite(LEDpin, LOW);
+  //digitalWrite(LEDpin, LOW);
   
   // BRUTTO We saves datas on a MicroSD so we could send them via LoRa only once a day (Solar Panel, so when light is max)
   // However if the battery ended or for every inconvenient crash we should send all the datas that probably wasn't able to send
@@ -85,9 +84,10 @@ void loop(void)
 { 
   
   // PIR sensor "number" of detections=detect and "number" of nothing=tool used to have the same time (a discapito della battery)
-  long count=0;
-  unsigned long detect=0;
-  unsigned long nothing=0;
+  int count=0;
+  int nothing=0;
+  int detect1=0;
+  int detect2=0;
   
   // Send command to all DS18B20 for temperature conversion
   sensors.requestTemperatures();
@@ -173,33 +173,48 @@ void loop(void)
   // In order for this to work we had to estimate the time on which the PIR was IN ALLARM (~we set it to 1.6s manually) and the time on which the alarm was BLOCKED (~3s from datasheet)
   // We used the program PIR_alarm_stop_time.ino and estimated the time through a mean of time shown at the Serial Monitor
   while (count<MaxCount){
-    PIRread = digitalRead(PIR_PIN);  // read PIR value
-    if (PIRread == HIGH) { 
-        detect=detect+1;
-      }
-      else{
+    PIRread1 = digitalRead(PIR_PIN1);  // read PIR1 value
+    PIRread2 = digitalRead(PIR_PIN2);  // read PIR2 value
+    if (PIRread1 == HIGH && PIRread2 == HIGH) { 
+        delay(100);
+        detect1=detect1+1;
+        detect2=detect2+1;
+      } else if (PIRread1 == HIGH && PIRread2 == LOW) {
+        delay(100);
+        detect1=detect1+1;
+        nothing=nothing+1;
+      } else if (PIRread1 == LOW && PIRread2 == HIGH) {
+        delay(100);
+        detect2=detect2+1;
+        nothing=nothing+1;
+      } else {
+        delay(100);
+        nothing=nothing+1; // These operations are useless, they are done in order to stop for the same amount of time in each else
         nothing=nothing+1;
       }
     count=count+1;
   }
 
-    Serial.println(detect/MaxCount);
-
+    Serial.println(detect1/MaxCount*333.3);
+    Serial.println(detect2/MaxCount*449.0);
+    
     
   // FILE_WRITE starts from the end of the file to read/save datas
   // BRUTTO Save in this order: the 5 temperature, the humidity and the percentual of activation of the PIR
-  myFile = SD.open("test.txt", FILE_WRITE);
+  myFile = SD.open("TestPr.txt", FILE_WRITE);
   
   // myFile = TRUE if it has been opened correctly
   if (myFile) {
     for (int i = 0; i < numDS18B20; i++) {
-    myFile.println(tempC[i]);
+    myFile.print(tempC[i]);
+    myFile.print(", ");
     }
-    //for (int i = 0; i < numDHT; i++) {
-    //myFile.println(humidity[i]);
-    //}
-    myFile.println(humidity);
-    myFile.println(detect/MaxCount);
+    myFile.print(humidity);
+    myFile.print(", ");
+    myFile.print(detect1/MaxCount*333.3);
+    myFile.print(", ");
+    myFile.print(detect2/MaxCount*449.0);
+    myFile.println(", ");
     
   myFile.close();
   }
